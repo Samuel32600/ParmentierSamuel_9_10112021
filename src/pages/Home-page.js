bcb7341810631b89c8458b135efdb2f39d8b93ed
Samuel32600 <samuel.parmentier@orange.fr>
2021-11-12 16:48:03 +0100

    mise en place du header Profil

--- src/pages/Home-page.js
+++ src/pages/Home-page.js
@@ -6,23 +6,49 @@ import '../styles/home-page.css'
 
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+        //initialisation des données
+        this.state = {
+            firstName: "",
+            calorieCount: "",
+            proteinCount: "",
+            carbohydrateCount: "",
+            lipidCount: "",
+        }
+    }
 
-    
     componentDidMount() {
-        axios.get("http://localhost:3001/user/18")
-        .then(function (request) {
+        const AdressLink = "http://localhost:3001/user/"
+        const UserIdentification = 12;
+        axios.get(AdressLink + UserIdentification)
 
-        
-            console.log(request.data.data.userInfos.firstName);
-          })
+            .then((request) => {
+                const Information = request.data.data;
+                //appel dans la console
+                // console.log(Information.userInfos.firstName)
+                // console.log(Information.keyData.calorieCount, "calories")
+                // console.log(Information.keyData.proteinCount, "protéines")
+                // console.log(Information.keyData.carbohydrateCount, "glucides")
+                // console.log(Information.keyData.lipidCount, "lipides")
+
+                //remplissage des données
+                this.setState(() => ({
+                    firstName: Information.userInfos.firstName,
+                }))
+            })
 
     }
 
 
     render() {
         return (
-            <div>
-                <h1 className='title'>Bonjour </h1>
+            <div className='container-home-page'>
+                <div className='box-title'>
+                    <h1 className='title'>Bonjour <span className='firstName'>{this.state.firstName}</span> </h1>
+                    <h2 className='subtitle'>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
+                </div>
+
 
             </div>
         )
